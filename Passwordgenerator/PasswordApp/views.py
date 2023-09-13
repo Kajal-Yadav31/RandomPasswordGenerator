520c1ff9fa1ab7ca153a5fe0a4ec0492b6b22ef7
kajal yadav <kajalyad31072001@gmail.com>
2023-09-13 13:56:49 +0530

    adding password generator logic

--- Passwordgenerator/PasswordApp/views.py
+++ Passwordgenerator/PasswordApp/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+import random
 
-# Create your views here.
+
+def generate(request):
+    return render(request, 'PasswordApp/generator.html')
+
+def password(request):
+
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+
+    if request.GET.get('uppercase'):
+        characters.entend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+    
+    if request.GET.get('special'):
+        characters.entend(list('!@#$%^&*()'))
+
+    if request.GET.get('numbers'):
+        characters.entend(list('0123456789'))
+
+    length = int(request.GET.get('length',12))
+
+    thepass = ''
+    for x in range(length):
+        thepass += random.choice(characters)
+
+    return render(request, 'PasswordApp/password.html', {'password' : thepass})
